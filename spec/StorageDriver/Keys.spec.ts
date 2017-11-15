3da4c8c901cdbcdd25fc5f0056071fc457955c1c
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-11-15 10:47:10 -0600

    Tests for storage driver keys made for non indexed item searches

---
+++ spec/StorageDriver/Keys.spec.ts
@@ -0,0 +1,125 @@
+import {ElectronStorage} from '../../src/StorageDriver';
+import {Datastore, IDatastore} from 'tedb';
+import {IStorageDriverExtended} from '../../src/types';
+import {AppDirectory} from '../../src/AppDirectory';
+import {ClearDirectory, UnlinkFile, AppendFile} from '../../src/utils';
+import {existsSync} from 'fs';
+const path = require('path');
+
+let Storage: IStorageDriverExtended;
+let TestDB: IDatastore;
+let dbAppName: string;
+let dbName: string;
+beforeAll(() => {
+    dbAppName = 'tedb-electron-storage-keys-tests';
+    dbName = 'keys-tests';
+    Storage = new ElectronStorage(dbAppName, dbName);
+    TestDB = new Datastore({storage: Storage});
+});
+
+afterAll(() => {
+    const toDelete = new AppDirectory(dbAppName);
+    ClearDirectory(toDelete.userData())
+        .then(() => {
+            console.log('deleted keys-tests');
+        })
+        .catch(console.log);
+});
+
+describe('testing keys', () => {
+    let Eversion;
+    let Bdir;
+    const docs: any = [
+        {a: 0},
+        {a: 1},
+        {a: 2},
+    ];
+    let file1;
+    let file2;
+    let file3;
+    let backupFile1;
+    let backupFile2;
+    let backupFile3;
+    let obj1;
+    let obj2;
+    let obj3;
+
+    test('inserting objects', () => {
+        expect.assertions(1);
+        return Promise.all(docs.map((doc) => {
+                return TestDB.insert(doc);
+            }))
+            .then((items) => {
+                Eversion = Storage.version;
+                Bdir = path.join(Storage.collectionPath, Eversion, 'states');
+                expect(items.length).toEqual(3);
+                file1 = path.join(Storage.collectionPath, `${items[0]._id}.db`);
+                file2 = path.join(Storage.collectionPath, `${items[1]._id}.db`);
+                file3 = path.join(Storage.collectionPath, `${items[2]._id}.db`);
+                backupFile1 = path.join(Bdir, `${items[0]._id}`);
+                backupFile2 = path.join(Bdir, `${items[1]._id}`);
+                backupFile3 = path.join(Bdir, `${items[2]._id}`);
+                obj1 = items[0];
+                obj2 = items[1];
+                obj3 = items[2];
+            });
+    });
+
+    test('finding all objects with empty search', () => {
+        // this will use keys because the search is empty
+        expect.assertions(1);
+        return TestDB.find({}).exec()
+            .then((objs) => {
+                objs = objs as any[];
+                expect(objs.length).toEqual(3);
+            });
+    });
+
+    test('removing a key from base location', () => {
+        expect.assertions(2);
+        return UnlinkFile(file1)
+            .then(() => TestDB.find({}).exec())
+            .then((objs) => {
+                objs = objs as any[];
+                expect(existsSync(file1)).toBeTruthy();
+                expect(objs.length).toEqual(3);
+            });
+    });
+
+    test('removing a key and making one unparcable', () => {
+        expect.assertions(2);
+        return UnlinkFile(file1)
+            .then(() => AppendFile(file2, '&'))
+            .then(() => TestDB.find({}).exec())
+            .then((objs) => {
+                objs = objs as any[];
+                expect(existsSync(file1)).toBeTruthy();
+                expect(objs.length).toEqual(3);
+            });
+    });
+
+    test('removing a key and making an items current and backup unparsable', () => {
+        expect.assertions(3);
+        return UnlinkFile(file1)
+            .then(() => AppendFile(file2, '&'))
+            .then(() => AppendFile(path.join(backupFile2, 'past'), '&'))
+            .then(() => TestDB.find({}).exec())
+            .then((objs) => {
+                objs = objs as any[];
+                expect(objs.length).toEqual(3); // one will be undefined;
+                expect(existsSync(file1)).toBeTruthy();
+                expect(existsSync(file2)).toBeFalsy();
+                return TestDB.insert(obj2);
+            });
+    });
+
+    test('not having any keys', () => {
+        expect.assertions(1);
+        Storage.allKeys = [];
+        return TestDB.find({}).exec()
+            .then((objs) => {
+                objs = objs as any[];
+                expect(objs.length).toEqual(3);
+            });
+    });
+});
--- src/StorageDriver/Keys.ts
+++ src/StorageDriver/Keys.ts
@@ -2,45 +2,95 @@ import {IStorageDriverExtended} from '../types';
 import {ReadDir, ReadFile, RmDir, UnlinkFile, EnsureDataFile, SafeWrite, safeParse, flattenStorageDriver} from '../utils';
 const path = require('path');
 
-const RemoveDirectoryAndFile = (dirLocation: string): Promise<string[]> => {
+const RemoveDirectoryAndFile = (base: string, dirLocation: string, key: string): Promise<string[]> => {
     return new Promise((resolve, reject) => {
-        return EnsureDataFile(dirLocation)
-            .then(() => UnlinkFile(path.join(dirLocation, 'past')))
-            .then(() => RmDir(dirLocation))
+        return EnsureDataFile(path.join(dirLocation, key))
+            .then(() => EnsureDataFile(path.join(base, `${key}.db`)))
+            .then(() => EnsureDataFile(path.join(dirLocation, key, 'past')))
+            .then(() => UnlinkFile(path.join(dirLocation, key, 'past')))
+            .then(() => RmDir(path.join(dirLocation, key)))
+            .then(() => UnlinkFile(path.join(base, `${key}.db`)))
             .then(() => resolve([]))
             .catch(reject);
     });
 };
 
+/**
+ * Write backup file data to current file location
+ * This is possible since the backup was not corrupted and the _id was retrievable.
+ * Using the id as the key to locate the current file path write its data to it and
+ * return the _id.
+ * @param fileData
+ * @param {string} baseLocation
+ * @returns {Promise<string[]>}
+ * @constructor
+ */
 const WriteBackupToBaseReturn = (fileData: any, baseLocation: string): Promise<string[]> => {
     return new Promise((resolve, reject) => {
         return EnsureDataFile(path.join(baseLocation, `${fileData._id}.db`))
             .then(() => SafeWrite(path.join(baseLocation, `${fileData._id}.db`), fileData))
-            .then(() => resolve([fileData._id as string]))
+            .then(() => resolve([fileData._id]))
             .catch(reject);
     });
 };
 
+interface IcomboRead {
+    key: string;
+    rawData: string;
+}
+const comboFileReadMethod = (dirLocation: string, dir: string | Buffer, key: string | Buffer): Promise<IcomboRead> => {
+    return new Promise((resolve, reject) => {
+        return ReadFile(path.join(dirLocation, dir, 'past'))
+            .then((rawData) => resolve({key: key.toString(), rawData: rawData.toString()}))
+            .catch(reject);
+    });
+};
+
+interface IcomboParse {
+    key: string;
+    parsedData: any;
+}
+const comboFileParseMethod = (obj: IcomboRead): Promise<IcomboParse> => {
+    return new Promise((resolve, reject) => {
+        return safeParse(obj.rawData)
+            .then((fileData) => resolve({key: obj.key, parsedData: fileData}))
+            .catch(reject);
+    });
+};
+
+/**
+ * Read the backup file to see if it is parsable
+ * if So -> return the key and write data to current location
+ * if Not -> delete backup. Can not delete current without reference
+ * @param {string} dirLocation
+ * @param {string} baseLocation
+ * @returns {Promise<string[][]>}
+ */
 const readBackupLocation = (dirLocation: string, baseLocation: string): Promise<string[][]> => {
     return new Promise((resolve, reject) => {
         return ReadDir(dirLocation)
             .then((directories) => {
-                const reg = new RegExp('index_');
-                const noIndexDirectories = directories.filter((dir) => reg.test(dir as string));
-                return Promise.all(noIndexDirectories.map((dir) => {
-                    return ReadFile(path.join(dirLocation, dir, 'past'));
+                // const reg = new RegExp('index_');
+                // const noIndexDirectories = directories.filter((dir) => reg.test(dir as string));
+
+                // no Need to filter out index_ or version since files being searched were already filtered
+                return Promise.all(directories.map((dir) => {
+                    return comboFileReadMethod(dirLocation, dir, dir);
+                    // return ReadFile(path.join(dirLocation, dir, 'past'));
                 }));
             })
-            .then((filesData: string[]) => {
-                return Promise.all(filesData.map((fd) => safeParse(fd)));
+            .then((filesRawData: IcomboRead[]) => {
+                return Promise.all(filesRawData.map((obj: IcomboRead) => comboFileParseMethod(obj)));
+                // return Promise.all(filesRawData.map((rawData) => safeParse(rawData)));
             })
-            .then((filesData: any[]): Promise<string[][]> => {
-                return Promise.all(filesData.map((fd) => {
-                    if (fd === false) {
-                        return RemoveDirectoryAndFile(dirLocation);
+            .then((filesData: IcomboParse[]): Promise<string[][]> => {
+                return Promise.all(filesData.map((fileData) => {
+                    if (fileData.parsedData === false) {
+                        // this means both current location and backup are unreadable. -> remove both
+                        return RemoveDirectoryAndFile(baseLocation, dirLocation, fileData.key);
                     } else {
                         // read backup location, write to baselocation then return _id;
-                        return WriteBackupToBaseReturn(fd, baseLocation);
+                        return WriteBackupToBaseReturn(fileData.parsedData, baseLocation);
                     }
                 }));
             })
@@ -57,20 +107,31 @@ const readKeysSafety = (baseLocation: string, dirLocation: string, Storage: ISto
         return ReadDir(baseLocation)
             .then((files) => {
                 const reg = new RegExp('index_');
-                const noIndexFiles = files.filter((file) => reg.test(file as string));
+                const vers = new RegExp('`v');
+                // don't try and read index files or the version directory
+                const noIndexFiles = files.filter((file) => {
+                    const thisFile = file.toString();
+                    if (!reg.test(thisFile) && !vers.test(thisFile)) {
+                        return file;
+                    }
+                });
+                // const noIndexFiles = files.filter((file) => reg.test(file as string));
                 return Promise.all(noIndexFiles.map((dbFile) => {
                     return ReadFile(path.join(baseLocation, dbFile));
                 }));
             })
-            .then((filesData: string[]) => {
-                return Promise.all(filesData.map((fd) => safeParse(fd)));
+            .then((filesRawData: string[]) => {
+                return Promise.all(filesRawData.map((rawData) => safeParse(rawData)));
             })
             .then((filesData: any[]): Promise<string[][][]> => {
                 return Promise.all(filesData.map((fd) => {
                     if (fd === false) {
+                        // search backup location for readable files for each file read its directory
+                        // -> dir location is -> appName/collection/version/states/ dir / past
                         return readBackupLocation(dirLocation, baseLocation);
                     } else {
-                        return (new Promise<string[][]>((res) => res([[]])));
+                        // resolve the actual key
+                        return (new Promise<string[][]>((res) => res([[fd._id]])));
                     }
                 }));
             })
@@ -78,7 +139,7 @@ const readKeysSafety = (baseLocation: string, dirLocation: string, Storage: ISto
                 const incomingKeys = flattenStorageDriver(keys);
                 let KEYS: string[] = [];
                 incomingKeys.forEach((key) => {
-                    KEYS = [...key];
+                    KEYS = [key, ...KEYS];
                 });
                 resolve([...Storage.allKeys, ...KEYS]);
             })
@@ -86,16 +147,45 @@ const readKeysSafety = (baseLocation: string, dirLocation: string, Storage: ISto
     });
 };
 
+const checkKeysLocations = (base: string, dir: string, Storage: IStorageDriverExtended): Promise<string[]> => {
+    return new Promise((resolve, reject) => {
+        return ReadDir(base)
+            .then((files) => {
+                const reg = new RegExp('index_');
+                const vers = new RegExp('`v');
+                // don't try and read index files or the version directory
+                const filteredFiles = files.filter((file) => {
+                    const thisFile = file.toString();
+                    if (!reg.test(thisFile) && !vers.test(thisFile)) {
+                        return file;
+                    }
+                });
+
+                if (filteredFiles.length === Storage.allKeys.length) {
+                    return Storage.allKeys;
+                } else {
+                    return readKeysSafety(base, dir, Storage);
+                }
+            })
+            .then(resolve)
+            .catch(reject);
+    });
+};
+
 export const Keys = (Storage: IStorageDriverExtended): Promise<any> => {
     return new Promise((resolve, reject) => {
+        const baseLocation = Storage.collectionPath;
+        const dirLocation = path.join(baseLocation, Storage.version, 'states');
         if (Storage.allKeys.length === 0) {
-            const baseLocation = Storage.collectionPath;
-            const dirLocation = path.join(baseLocation, Storage.version, 'states');
             return readKeysSafety(baseLocation, dirLocation, Storage)
                 .then(resolve)
                 .catch(reject);
         } else {
-            resolve(Storage.allKeys);
+            // should check current directories files length should match keys length
+            // resolve(Storage.allKeys);
+            return checkKeysLocations(baseLocation, dirLocation, Storage)
+                .then(resolve)
+                .catch(reject);
         }
     });
 };
