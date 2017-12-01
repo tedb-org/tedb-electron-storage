ab60f425748501376c54973335ca2480624e88da
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-12-01 14:41:14 -0600

    small commit possible use?

--- src/utils/safeParse.ts
+++ src/utils/safeParse.ts
@@ -2,15 +2,15 @@
 export const safeParse = (data: string): Promise<any> => {
     return new Promise((resolve, reject) => {
         try {
-            const json = JSON.parse(data);
+            let json = JSON.parse(data);
             // this could be used to save and read back items as buffers
-            /*if (json.hasOwnProperty('type')) {
+            if (json.hasOwnProperty('type')) {
                 if (json.type === 'Buffer') {
                     const buff = Buffer.from(JSON.parse(data).data);
                     json = JSON.stringify(buff);
                     json = JSON.parse(json);
                 }
-            }*/
+            }
             resolve(json);
         } catch (e) {
             resolve(false);
